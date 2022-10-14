8da9d31dad2a4f6d430f8902e592c3acf505db6c
João Ricardo Joaquim <joaorjoaquim@gmail.com>
2022-10-14 16:33:42 -0300

    filelist

--- src/components/FileList/index.js
+++ src/components/FileList/index.js
@@ -3,40 +3,55 @@ import { Container, FileInfo, Preview } from './styles';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import { MdCheckCircle, MdError, MdLink } from 'react-icons/md';
 
-const FileList = () => (
+const FileList = ({ files }) => (
     <Container>
-        <li>
-            <FileInfo>
-                <Preview src="https://st.depositphotos.com/1010338/2099/i/450/depositphotos_20999947-stock-photo-tropical-island-with-palms.jpg" />
+        {files.map((uploadedFile) => (
+            <li>
+                <FileInfo>
+                    <Preview src={uploadedFile.preview} />
+                    <div>
+                        <strong>{uploadedFile.name}</strong>
+                        <span>
+                            {uploadedFile.readableSize}
+                            <button onClick={() => {}}>Excluir</button>
+                        </span>
+                    </div>
+                </FileInfo>
                 <div>
-                    <strong>Profile.png</strong>
-                    <span>
-                        64kb
-                        <button onClick={() => {}}>Excluir</button>
-                    </span>
-                </div>
-            </FileInfo>
-            <div>
-                <CircularProgressbar
-                    styles={{
-                        root: { width: 24 },
-                        path: { stroke: '#7159c1' },
-                    }}
-                    strokeWidth={10}
-                    value={60}
-                />
+                    {!uploadedFile.uploaded && !uploadedFile.error && (
+                        <CircularProgressbar
+                            styles={{
+                                root: { width: 24 },
+                                path: { stroke: '#7159c1' },
+                            }}
+                            strokeWidth={10}
+                            value={uploadedFile.progress}
+                        />
+                    )}
 
-                <a
-                    href="https://st.depositphotos.com/1010338/2099/i/450/depositphotos_20999947-stock-photo-tropical-island-with-palms.jpg"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <MdLink style={{ marginRight: 8 }} size={24} color="#222" />
-                </a>
-                <MdCheckCircle size={24} color="#78e5d5" />
-                <MdError size={24} color="#e57878" />
-            </div>
-        </li>
+                    {uploadedFile.url && (
+                        <a
+                            href="https://st.depositphotos.com/1010338/2099/i/450/depositphotos_20999947-stock-photo-tropical-island-with-palms.jpg"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <MdLink
+                                style={{ marginRight: 8 }}
+                                size={24}
+                                color="#222"
+                            />
+                        </a>
+                    )}
+
+                    {uploadedFile.uploaded && (
+                        <MdCheckCircle size={24} color="#78e5d5" />
+                    )}
+                    {uploadedFile.error && (
+                        <MdError size={24} color="#e57878" />
+                    )}
+                </div>
+            </li>
+        ))}
     </Container>
 );
 
