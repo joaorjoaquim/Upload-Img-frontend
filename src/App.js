29ccd3a42fa6bf2e7ebd39dfee1632e48ba4d557
João Ricardo Joaquim <joaorjoaquim@gmail.com>
2022-10-14 16:33:50 -0300

    more dependencies

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
+import React, { Component } from 'react';
+import { uniqueId } from 'lodash';
+import { filesize } from 'filesize';
 import GlobalStyle from './styles/global';
 import { Container, Content } from './styles';
 import Upload from './components/Upload';
 import FileList from './components/FileList';
 
-function App() {
-    return (
-        <Container>
-            <Content>
-                <Upload />
-                <FileList />
-            </Content>
-            <GlobalStyle />
-        </Container>
-    );
-}
+class App extends Component {
+    state = {
+        uploadedFiles: [],
+    };
+    handleUpload = (files) => {
+        const uploadedFiles = files.map((file) => ({
+            file,
+            id: uniqueId(),
+            name: file.name,
+            readableSize: filesize(file.size),
+            preview: URL.createObjectURL(file),
+            progress: 0,
+            uploaded: false,
+            error: false,
+            url: null,
+        }));
 
+        this.setState({
+            uploadedFiles: this.state.uploadedFiles.concat(uploadedFiles),
+        });
+    };
+
+    render() {
+        const { uploadedFiles } = this.state;
+
+        return (
+            <Container>
+                <Content>
+                    <Upload onUpload={this.handleUpload} />
+                    {!!uploadedFiles.length && (
+                        <FileList files={uploadedFiles} />
+                    )}
+                </Content>
+                <GlobalStyle />
+            </Container>
+        );
+    }
+}
 export default App;
